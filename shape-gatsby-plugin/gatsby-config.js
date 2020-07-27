a520b769dd35c49680badf726af78767e43732b6
Greg Smith <greg@takeshape.io>
2020-07-27 16:27:46 -0700

    progress

--- shape-gatsby-plugin/gatsby-config.js
+++ shape-gatsby-plugin/gatsby-config.js
@@ -6,21 +6,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "gatsby-source-graphql",
-      options: {
-        typeName: "TS",
-        fieldName: "takeshape",
-        url: `https://api.takeshape.io/project/${
-          process.env.TAKESHAPE_PROJECT
-        }/graphql`,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.TAKESHAPE_TOKEN}`
-        },
-        fetchOptions: {}
-      }
-    },
-    {
       resolve: "@takeshape/gatsby-source-takeshape",
       options: {}
     }
