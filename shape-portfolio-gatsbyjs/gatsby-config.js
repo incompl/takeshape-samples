66dcb2af9a9b5da6c45bffd605ec2f4f6069f706
Greg Smith <greg@takeshape.io>
2020-07-30 15:21:22 -0700

    It works!

--- shape-portfolio-gatsbyjs/gatsby-config.js
+++ shape-portfolio-gatsbyjs/gatsby-config.js
@@ -19,18 +19,18 @@ module.exports = {
       }
     },
     {
-      resolve: "gatsby-source-graphql",
+      resolve: "@takeshape/gatsby-source-takeshape",
       options: {
-        typeName: "TS",
-        fieldName: "takeshape",
-        url: `https://api.takeshape.io/project/${
-          process.env.TAKESHAPE_PROJECT
-        }/graphql`,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.TAKESHAPE_TOKEN}`
-        },
-        fetchOptions: {}
+        // typeName: "TS",
+        // fieldName: "takeshape",
+        // url: `https://api.takeshape.io/project/${
+        //   process.env.TAKESHAPE_PROJECT
+        // }/graphql`,
+        // headers: {
+        //   "Content-Type": "application/json",
+        //   Authorization: `Bearer ${process.env.TAKESHAPE_TOKEN}`
+        // },
+        // fetchOptions: {}
       }
     },
     "gatsby-plugin-offline"
